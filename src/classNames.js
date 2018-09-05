031dde97b5c1ee3241636e702d62206a4b893556
Jovi De Croock <jovi@codifly.be>
2018-09-05 07:55:54 +0200

    fix: build the library

--- src/classNames.js
+++ src/classNames.js
@@ -1,4 +1,4 @@
-import styles from "@-ui/flexboxgrid/dist/flexboxgrid.css";
+import styles from '@a-ui/flexboxgrid/dist/flexboxgrid.css';
 
 export default function getClass(className) {
   return styles && styles[className] ? styles[className] : className;
